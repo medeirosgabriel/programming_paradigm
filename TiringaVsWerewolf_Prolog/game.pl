b45f4ec513695259aa8ac448b9ab9e66331c1247
medeirosgabriel <gabriel.medeiros@ccc.ufcg.edu.br>
2020-11-14 12:44:35 -0300

    prolog

---
+++ TiringaVsWerewolf_Prolog/game.pl
@@ -0,0 +1,16 @@
+﻿
+:- include('util.pl').
+:- include('tiringa.pl').
+:- include('maps.pl').
+
+prepareGame :- 
+    findElement(Matrix, 0, "T", TPosition),
+    findElement(Matrix, 0, "W", WPosition),
+    getMaps(Maps),
+    length(Maps, Size),
+    random(0, Size, N),
+    nth0(N, Maps, M),
+    startGame(M, TPosition, WPosition).
+
+startGame(M, TP, WP) :- 
+    
