aa2612138a3cf8dc84ac467e1f8b226e3e54a6c3
medeirosgabriel <gabriel.medeiros@ccc.ufcg.edu.br>
2020-11-14 23:39:06 -0300

    ranking

--- TiringaVsWerewolf_Prolog/main.pl
+++ TiringaVsWerewolf_Prolog/main.pl
@@ -1,7 +1,10 @@
 ﻿players(R) :-
     read_file_to_string("ranking.txt", Players, []),
     split_string(Players, "\n", "", PlayersList),
-    splitPlayer(PlayersList, R).
+    nth0(0, PlayersList, Verif),
+    splitPlayer(PlayersList, PlayersList2),
+    sort(1, @<, PlayersList2, SortedPlayers),
+    nth0(0, SortedPlayers, R).
 
 splitPlayer([], R) :- R = [].
 splitPlayer([H|T], R) :-
@@ -9,7 +12,7 @@ splitPlayer([H|T], R) :-
     nth0(0, Player, Name), nth0(1, Player, N),
     number_codes(Mov, N),
     L = [Name, Mov],
-    splitPlayer([T], NP),
+    splitPlayer(T, NP),
     append([L], NP, R).
 
 test :-
