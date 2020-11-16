0e3a8eb50a5168f559065327a9f47959a9a9cc39
medeirosgabriel <gabriel.medeiros@ccc.ufcg.edu.br>
2020-11-16 15:43:52 -0300

    quantion_3 and question_4

---
+++ Prolog Solved Questions/question_3.pl
@@ -0,0 +1,22 @@
+﻿main :- 
+    read(L),
+    length(L, Size),
+    getMiddle(Size, M),
+    list_sum_by_index(L, M, Left),
+    list_sum_by_index(L, Size, Total),
+    Right is Total - Left,
+    message(Left, Right), halt.
+    
+message(Left, Right) :-
+    (Left >= Right -> write("esquerda");
+    write("direita")).
+    
+getMiddle(Size, R) :-
+    (mod(Size, 2) =:= 0 -> R is Size/2;
+    NS is (Size + 1)/2 - 1, R is NS).
+    
+list_sum_by_index(_, 0, S) :- S = 0.
+list_sum_by_index([H|T], I, Total) :- 
+    NI is I - 1,
+    list_sum_by_index(T, NI, S),
+    Total is S + H.
---
+++ Prolog Solved Questions/question_4.pl
@@ -0,0 +1,11 @@
+﻿main :-
+    read(L),
+    read(I),
+    removeElement(L, I, R),
+    write(R), halt.
+    
+removeElement([_|T], 0, R) :- R = T.
+removeElement([H|T], I, R) :-
+    NI is I - 1,
+    removeElement(T, NI, PL),
+    append([H], PL, R).
